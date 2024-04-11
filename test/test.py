693b088aa42429e72fb9eb164f0c23d5957485ed
Locochoco <ivanrwpf@gmail.com>
2024-04-11 19:49:42 -0300

    add testbench

--- test/test.py
+++ test/test.py
@@ -9,7 +9,6 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
   dut._log.info("Start")
   
-  # Our example module doesn't use clock and reset, but we show how to use them here anyway.
   clock = Clock(dut.clk, 10, units="us")
   cocotb.start_soon(clock.start())
 
@@ -21,12 +20,16 @@ async def test_project(dut):
   dut.rst_n.value = 0
   await ClockCycles(dut.clk, 10)
   dut.rst_n.value = 1
-
-  # Set the input values, wait one clock cycle, and check the output
-  dut._log.info("Test")
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
-
-  await ClockCycles(dut.clk, 1)
-
-  assert dut.uo_out.value == 50
+  
+  # Cheks if the '+' operation cycle is working
+  dut._log.info("Non Instr Test")
+  loops = 20
+  for i in range(loops):
+    await ClockCycles(dut.clk, 1)
+    # fetch instr cycle
+    dut._log.info(f"PC = {int(dut.uo_out.value)}")
+    dut._log.info(f"Data = {int(dut.uio_out.value)}")
+    dut._log.info(f"DataEn = {dut.uio_oe.value}")
+    #assert dut.uo_out.value == i
+    dut.uio_in.value = 43 # invalid instr
+    await ClockCycles(dut.clk, 1)
