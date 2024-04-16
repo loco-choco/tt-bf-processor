132540805b44df6fce0018dc574a0a81a094c0fd
Locochoco <ivanrwpf@gmail.com>
2024-04-16 12:37:53 -0300

    updating tests

--- test/test.py
+++ test/test.py
@@ -14,18 +14,18 @@ async def test_project(dut):
 
   # Reset
   dut._log.info("Reset")
-  dut.ena.value = 1
+  dut.ena.value = 0
   dut.ui_in.value = 0
   dut.uio_in.value = 0
   dut.rst_n.value = 0
   await ClockCycles(dut.clk, 10)
   dut.rst_n.value = 1
+  dut.ena.value = 1
   
   # Cheks if the '+' operation cycle is working
   dut._log.info("Non Instr Test")
   loops = 20
   for i in range(loops):
-    await ClockCycles(dut.clk, 1)
     # fetch instr cycle
     dut._log.info(f"PC = {int(dut.uo_out.value)}")
     dut._log.info(f"Data = {int(dut.uio_out.value)}")
